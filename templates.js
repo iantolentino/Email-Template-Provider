d85e6d684b577a9d1f8935fde9e0b8420eb1d8d6
Ian Tolentino <165245557+iantolentino@users.noreply.github.com>
2025-12-26 20:43:37 +0800

    Update templates.js

--- templates.js
+++ templates.js
@@ -4,7 +4,7 @@ const EMAIL_CONSTANTS = {
   TEAM_SALUTATION: 'Dear Team,\n\n', 
   DEPARTMENT_SALUTATION: 'Dear [Department] Team,\n\n',
   ALL_STAFF_SALUTATION: 'Dear All,\n\n',
-  EMERGENCY_SALUTATION: 'URGENT NOTIFICATION\n\n',
+  EMERGENCY_SALUTATION: 'URGENT NOTIFICATION\n\n', 
     
   // Basic signatures
   SHORT_SIGNATURE: '\n\nRegards,\n[Name]', 
@@ -5208,6 +5208,7 @@ ${EMAIL_CONSTANTS.DEPARTMENT_SIGNATURE}`
   }
  
 ];
+
 
 
 
